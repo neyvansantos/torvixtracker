7644ecb550844504e20b4106be067a697b280013
Neyvan <neyvanivan@gmail.com>
2026-04-30 21:00:43 -0300

    Restore production checkout price

--- website/src/config/product.ts
+++ website/src/config/product.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2026 Neyvan Santos. Todos os direitos reservados.
 export const PRODUCT_NAME = "Torvix Tracker";
-export const PRICE_TEXT = "R$0,30";
-export const PRICE_FULL = "Pagamento único de R$0,30";
-export const PRICE_AMOUNT = 0.3;
+export const PRICE_TEXT = "R$19,99";
+export const PRICE_FULL = "Pagamento único de R$19,99";
+export const PRICE_AMOUNT = 19.99;
