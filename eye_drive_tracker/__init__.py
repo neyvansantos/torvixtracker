df6b865b4f8c051b96ba1e8fb4b8d70d8ddae6f8
Neyvan <neyvanivan@gmail.com>
2026-05-05 19:39:15 -0300

    Release 0.1.5

--- eye_drive_tracker/__init__.py
+++ eye_drive_tracker/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2026 Torvix Tracker. Todos os direitos reservados.
 __app_name__ = "Torvix Tracker"
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __copyright__ = "Copyright (c) 2026 Torvix Tracker. Todos os direitos reservados."
